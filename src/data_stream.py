f5fa4501d9238877c52828d18d610e7b550c0e69
Bhaskar-786 <101655442+Bhaskar-786@users.noreply.github.com>
2024-09-28 18:58:26 +0530

    Update data_stream.py

--- src/data_stream.py
+++ src/data_stream.py
@@ -10,20 +10,18 @@ def generate_data_stream(length=1000, seasonal_period=80, noise_level=0.1):
     seasonal += 0.5 * np.sin(4 * np.pi * time / seasonal_period)   
     seasonal += 0.25 * np.sin(8 * np.pi * time / seasonal_period) 
     
-    # Random noise
+ 
     noise = np.random.normal(0, noise_level, length)
-    
-    # Gradual upward trend
     trend = np.linspace(0,2, length)
     
-    # Occasional spikes
+    # Adding spikes
     spikes = np.zeros(length)
     num_spikes = np.random.randint(1, 5)   
     spike_indices = np.random.choice(length, num_spikes, replace=False)
     spike_magnitudes = np.random.uniform(5, 10, num_spikes)   
     spikes[spike_indices] = spike_magnitudes
     
-    # Combining all components
+   
     data = seasonal + noise + trend + spikes
 
     #normalize data
